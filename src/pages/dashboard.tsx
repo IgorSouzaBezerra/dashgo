f02026be00321afb27ae81a274ebf827c584bd38
Igor <igor_csns_s2@hotmail.com>
2021-09-30 22:36:03 -0300

    correct typing

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
-
+import { ApexOptions } from 'apexcharts';
 import dynamic from "next/dynamic";
 
 import { Header } from "../components/Header";
@@ -9,52 +9,37 @@ const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
 })
 
-const options = {
+const options: ApexOptions = {
   chart: {
-    toolbar: {
-      show: false
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[500],
+    toolbar: { show: false },
+    zoom: { enabled: false },
+    foreColor: theme.colors.gray['500'],
   },
-  grid: { 
-    show: false,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  tooltip: {
-    enabled: false
-  },
-   xaxis: {
+  grid: { show: false },
+  dataLabels: { enabled: false },
+  tooltip: { enabled: false },
+  xaxis: {
     type: 'datetime',
-    axisBorder: {
-      color: theme.colors.gray[600]
-    },
-    axisTicks: {
-      color: theme.colors.gray[600]
-    },
+    axisBorder: { color: theme.colors.gray['600'] },
+    axisTicks: { color: theme.colors.gray['600'] },
     categories: [
-      '2021-03-18T00:00:00.000Z',
-      '2021-03-19T00:00:00.000Z',
-      '2021-03-20T00:00:00.000Z',
-      '2021-03-21T00:00:00.000Z',
-      '2021-03-22T00:00:00.000Z',
-      '2021-03-23T00:00:00.000Z',
-      '2021-03-24T00:00:00.000Z',
+      '2021-04-02T00:00:00:00.000Z',
+      '2021-04-03T00:00:00:00.000Z',
+      '2021-04-04T00:00:00:00.000Z',
+      '2021-04-05T00:00:00:00.000Z',
+      '2021-04-06T00:00:00:00.000Z',
+      '2021-04-07T00:00:00:00.000Z',
     ],
-   },
-   fill: {
-     opaticy: 0.3,
-     type: 'gradient',
-     gradient: {
-       shade: 'dark',
-       opacityFrom: '0.7',
-       opacityTo: '0.3'
-     }
-   },
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
 };
 
 const series = [
